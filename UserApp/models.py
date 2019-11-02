fc8ca50ea2569a60c4be9fe921a32ed54c02ff82
yh <you@example.com>
2019-11-02 17:52:26 +0800

    User模型初始化

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
-# Create your models here.
+
+class UserappUser(models.Model):
+    phonenum = models.CharField(unique=True, max_length=15)
+    nickname = models.CharField(max_length=20)
+    sex = models.CharField(max_length=8)
+    birthday = models.DateField()
+    avatar = models.CharField(max_length=256)
+    location = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'UserApp'
+
